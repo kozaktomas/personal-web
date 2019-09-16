2d14eea26f574bb87dd385b823004104f662618c
Tomas Kozak <tomas.kozak@kiwi.com>
2019-09-16 19:01:58 +0200

    Add google analytics

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -26,6 +26,7 @@ class HomepagePresenter extends BasePresenter
 	{
 		parent::beforeRender();
 		$this->template->age = $this->getAge();
+		$this->template->googleAnalytics = !(bool)\getenv('DEV');
 	}
 
 	/**
