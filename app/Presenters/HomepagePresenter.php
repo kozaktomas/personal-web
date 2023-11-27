af01073753d889b6f15767719b79a2066eb9f66d
Tomas Kozak <kozak@talko.cz>
2023-11-27 21:30:13 +0100

    Fix sitemap.xml namespace

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -125,7 +125,7 @@ final class HomepagePresenter extends BasePresenter
             namespaceOrPrefix: 'ws',
         );
 
-        $xml->addAttribute('xmlns:xmlns', 'http://url.to.namespace');
+        $xml->addAttribute('xmlns:xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
         foreach ($pages as $page) {
             $xml
